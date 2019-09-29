747ec221c47c928362707803629a925ec2bbfb72
PaulAlarcon <37510926+PaulAlarcon@users.noreply.github.com>
2019-09-29 12:34:34 -0400

    Test.py

---
+++ test.py
@@ -0,0 +1,32 @@
+
+def tokenize(text):
+    mc = {}
+    for w in text.split():
+        if w in mc.keys():
+            mc[w] = mc[w] + 1
+        else:
+            mc[w] = 1
+
+    return mc
+
+
+t = open("test.txt")
+t_read = t.read().lower()
+
+t_processed = "";
+
+for line in t_read.split("\n"):
+    line = "<s> " + line + " </s> \n"
+    t_processed += line
+
+t_processed_padded = t_processed;
+
+map_count = tokenize(t_processed)
+
+for key in map_count.keys():
+    if map_count[key] == 1:
+        word = " "+key+" "
+        t_processed = t_processed.replace(key, " <unk> ")
+new_map_count = tokenize(t_processed)
+
+print(len(t_processed.split()))
