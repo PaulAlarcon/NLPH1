8343de6f46a7b123b26edf4f49a6aa8f1f492062
Hamidur Rahman <39712889+HamidurRahman1@users.noreply.github.com>
2019-09-29 12:47:52 -0400

    myModifications

--- HAMIDUR.py
+++ HAMIDUR.py
@@ -1,1 +1,25 @@
-print("Hey Hamid :D")
+def tokenize(all_texts_lines):
+    word_map = {}
+    for l in all_texts_lines:
+        words = l.split()
+        for w in words:
+            if w in word_map.keys():
+                word_map[w] += 1
+            else:
+                word_map[w] = 1
+    return word_map
+
+
+def read(file):
+    f = open(file)
+    all = f.readlines()
+    newFile = []
+    for l in all:
+        newFile.append("<s> " + l.rstrip().lstrip().lower() + " </s>")
+    return newFile
+
+
+updatedLines = read("test.txt")
+updatedMap = tokenize(updatedLines)
+print("Unique", len(updatedMap))
+print("Total tokens", sum(updatedMap.values()))
