5375c67c1ff6704e632a39b23f2086a01ced50ff
Hamidur Rahman <39712889+HamidurRahman1@users.noreply.github.com>
2019-09-29 13:04:20 -0400

    Update HAMIDUR.py

--- HAMIDUR.py
+++ HAMIDUR.py
@@ -1,4 +1,3 @@
-
 def tokenize(all_texts_lines):
     word_map = {}
     for l in all_texts_lines:
@@ -9,6 +8,18 @@ def tokenize(all_texts_lines):
             else:
                 word_map[w] = 1
     return word_map
+
+
+def replace_1_Occur(oldMap, oldUpdatedLines):
+    newLines = list(oldUpdatedLines)
+    newList = []
+    for l in newLines:
+        words = l.split()
+        for w in words:
+            if oldMap[w] == 1:
+                a = l.replace(w, "<unk>")
+                newList.append(a)
+    return newList
 
 
 def read(file):
@@ -25,9 +36,9 @@ def write(updatedLines, newFileName):
         f.write(l+"\n")
 
 
-updatedLines = read("test.txt")
+updatedLines = read("train.txt")
 updatedMap = tokenize(updatedLines)
-write(updatedLines, "mod")
-
+write(updatedLines, "mod.txt")
+print(replace_1_Occur(updatedMap, updatedLines))
 print("Unique", len(updatedMap))
 print("Total tokens", sum(updatedMap.values()))
